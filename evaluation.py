0c9aaa6055c23118f15e2cb54e81faa6e74c58e0
t170815518 <christang_1023@outlook.com>
2020-08-29 22:12:52 +0800

    Correct evaluation process
    (cherry picked from commit 81f74537220f4205fcb78316caf62aa02fff37c4)

--- evaluation.py
+++ evaluation.py
@@ -1,101 +1,111 @@
-import logging
+'''
+This module is for evaluation on link prediction result
+'''
 
+import logging
 import numpy as np
 import torch
 from torch.nn import functional as F
+from random import sample
 
 
 log = logging.getLogger()
 
 
-def ranking_and_hits(model, dev_rank_batcher, batch_size, name, silent=False):
+def ranking_and_hits(model, dev_rank_batcher, batch_size, name, isSilent=False, testRate1=1,
+                     testSize2=0.3):
+    '''
+    Evaluate Mean rank and Hits@10 based on data in dev_rank_batcher whose size is batch_size
+    :param model: torch module, trained model
+    :param dev_rank_batcher: DataStreamer object, including tuples to evaluate
+    :param batch_size: int, the size of evaluation batch
+    :param name: string, to format the display message
+    :param isSilent: boolean, True means to verbose the evaluation result
+    :return: float, mean rank
+    '''
+    printDisplayMessage(name)
+
+    # initialize
+    hits = []
+    ranks = []
+
+    for i, str2var in enumerate(dev_rank_batcher):  # iter over batcher containing multiple batches
+        e1, e2, e2_multi1, e2_multi2, rel, rel_reverse = loadInfoFromBatcher(str2var)
+        pred1, pred2 = predictProbability(e1, e2, model, rel, rel_reverse)
+
+        # get the data
+        pred1, pred2 = pred1.data, pred2.data
+        e1, e2 = e1.data, e2.data
+        e2_multi1, e2_multi2 = e2_multi1.data, e2_multi2.data
+
+        # sort the prediction tensor along the row
+        max_values, argsort1 = torch.sort(pred1, 1, descending=True)
+        max_values, argsort2 = torch.sort(pred2, 1, descending=True)
+
+        for j in range(batch_size):  # iterate over batch with the size of 128
+            # find the positive entities
+            postiveEntities1 = e2_multi1[j][e2_multi1[j] != -1].long().tolist()
+
+            # sample positive entities to save evaluation time
+            if len(postiveEntities1) > 3:
+                postiveEntities1 = sample(postiveEntities1, 3)
+            postiveEntities2 = e2_multi2[j][e2_multi2[j] != -1].long().tolist()
+            if len(postiveEntities2) > 3:
+                postiveEntities2 = sample(postiveEntities2, 3)
+
+            # find the rank of the target entities
+            hit1 = 0
+            for pos_e1 in postiveEntities1:
+                rank1 = (argsort1[j] == pos_e1).nonzero().item()
+                if rank1 < 9:
+                    hit1 += 1
+                ranks.append(rank1+1)  # rank+1, since the lowest rank is rank 1 not rank 0
+            hit1 /= 10
+
+            hit2 = 0
+            for pos_e2 in postiveEntities2:
+                rank2 = (argsort2[j] == pos_e2).nonzero().item()
+                if hit2 < 9:
+                    hit2 += 1
+                ranks.append(rank2+1)  # rank+1, since the lowest rank is rank 1 not rank 0
+            hit2 /= 10
+
+            hits.extend([hit1, hit2])
+
+    if not isSilent:
+        meanRank = np.mean(ranks)
+        hits10 = np.mean(hits)
+        log.info('Hits @10: %f', hits10)
+        log.info('Mean rank: %f', meanRank)
+        # log.info('Mean reciprocal rank: %f', np.mean(1. / np.array(ranks)))
+
+    return meanRank
+
+
+def predictProbability(e1, e2, model, rel, rel_reverse):
+    pred1_ = model.forward(e1, rel)
+    pred2_ = model.forward(e2, rel_reverse)
+
+    # sigmoid is applied in loss function, here we do it manually to get positive predictions
+    pred1 = F.sigmoid(pred1_)
+    pred2 = F.sigmoid(pred2_)
+    return pred1, pred2
+
+
+def loadInfoFromBatcher(str2var):
+    e1 = str2var['e1']
+    e2 = str2var['e2']
+    rel = str2var['rel']
+    rel_reverse = str2var['rel_eval']
+    e2_multi1 = str2var['e2_multi1'].float()
+    e2_multi2 = str2var['e2_multi2'].float()
+    return e1, e2, e2_multi1, e2_multi2, rel, rel_reverse
+
+
+def printDisplayMessage(name):
     log.info('')
     log.info('-' * 50)
     log.info(name)
     log.info('-' * 50)
     log.info('')
-
-    hits_left = []
-    hits_right = []
-    hits = []
-    ranks = []
-    ranks_left = []
-    ranks_right = []
-
-    for i in range(10):
-        hits_left.append([])
-        hits_right.append([])
-        hits.append([])
-
-    for i, str2var in enumerate(dev_rank_batcher):
-        e1 = str2var['e1']
-        e2 = str2var['e2']
-        rel = str2var['rel']
-        rel_reverse = str2var['rel_eval']
-        e2_multi1 = str2var['e2_multi1'].float()
-        e2_multi2 = str2var['e2_multi2'].float()
-        pred1_ = model.forward(e1, rel)
-        pred2_ = model.forward(e2, rel_reverse)
-
-        # sigmoid is applied in loss function
-        # here we do it manually to get positive predictions
-        pred1 = F.sigmoid(pred1_)
-        pred2 = F.sigmoid(pred2_)
-
-        pred1, pred2 = pred1.data, pred2.data
-        e1, e2 = e1.data, e2.data
-        e2_multi1, e2_multi2 = e2_multi1.data, e2_multi2.data
-        for i in range(batch_size):
-            filter1 = e2_multi1[i][e2_multi1[i] != -1].long()
-            filter2 = e2_multi2[i][e2_multi2[i] != -1].long()
-
-            # save the prediction that is relevant
-            target_value1 = pred1[i, e2[i, 0]]
-            target_value2 = pred2[i, e1[i, 0]]
-
-            # zero all known cases (this are not interesting)
-            # this corresponds to the filtered setting
-            pred1[i][filter1] = 0.0
-            pred2[i][filter2] = 0.0
-            # write base the saved values
-            pred1[i][e2[i]] = target_value1
-            pred2[i][e1[i]] = target_value2
-
-        # sort and rank
-        max_values, argsort1 = torch.sort(pred1, 1, descending=True)
-        max_values, argsort2 = torch.sort(pred2, 1, descending=True)
-        argsort1 = argsort1.cpu().numpy()
-        argsort2 = argsort2.cpu().numpy()
-        for i in range(batch_size):
-            # find the rank of the target entities
-            rank1 = np.where(argsort1[i] == e2.data.cpu().numpy()[i])[0]
-            rank2 = np.where(argsort2[i] == e1.data.cpu().numpy()[i])[0]
-            # rank+1, since the lowest rank is rank 1 not rank 0
-            ranks.append(rank1+1)
-            ranks_left.append(rank1+1)
-            ranks.append(rank2+1)
-            ranks_right.append(rank2+1)
-
-            # this could be done more elegantly, but here you go
-            for hits_level in range(10):
-                if rank1 <= hits_level:
-                    hits[hits_level].append(1.0)
-                    hits_left[hits_level].append(1.0)
-                else:
-                    hits[hits_level].append(0.0)
-                    hits_left[hits_level].append(0.0)
-
-                if rank2 <= hits_level:
-                    hits[hits_level].append(1.0)
-                    hits_right[hits_level].append(1.0)
-                else:
-                    hits[hits_level].append(0.0)
-                    hits_right[hits_level].append(0.0)
-
-    if not silent:
-        log.info('Hits @10: %f', np.mean(hits[9]))
-        log.info('Mean rank: %f', np.mean(ranks))
-        log.info('Mean reciprocal rank: %f', np.mean(1. / np.array(ranks)))
-
-    return np.mean(ranks)
 
